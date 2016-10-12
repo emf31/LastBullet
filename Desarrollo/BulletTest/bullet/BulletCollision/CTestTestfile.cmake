eb526e55a742d916f25f83d04e08e9a605c1af54
Juliyo <julio17795@hotmail.com>
2016-10-13 00:56:24 +0200

    Ahora bullet esta enlazado con librerias que he conseguido compilar

--- Desarrollo/BulletTest/bullet/BulletCollision/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Enrique/Desktop/bullet3-2.84/src/BulletCollision
-# Build directory: C:/Users/Enrique/Desktop/cmake/src/BulletCollision
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- Desarrollo/BulletTest/bullet/LinearMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Enrique/Desktop/bullet3-2.84/src/LinearMath
-# Build directory: C:/Users/Enrique/Desktop/cmake/src/LinearMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
