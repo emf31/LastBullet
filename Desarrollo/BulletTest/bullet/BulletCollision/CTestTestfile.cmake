c5d4f1d238b4f54a2f4fba5d9f70a2f20267290c
Enrique Moltó <kike.molto@gmail.com>
2016-10-12 13:19:55 +0200

    Fisicas + Irrlicht configurado

---
+++ Desarrollo/BulletTest/bullet/BulletCollision/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Enrique/Desktop/bullet3-2.84/src/BulletCollision
+# Build directory: C:/Users/Enrique/Desktop/cmake/src/BulletCollision
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Desarrollo/BulletTest/bullet/BulletDynamics/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Enrique/Desktop/bullet3-2.84/src/BulletDynamics
+# Build directory: C:/Users/Enrique/Desktop/cmake/src/BulletDynamics
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
