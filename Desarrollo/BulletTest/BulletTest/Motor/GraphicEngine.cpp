c25b8ed6a69472c68041a9b965e593a9d2be7f8b
Juliyo <julio17795@hotmail.com>
2016-11-02 23:06:12 +0100

    Añadida mirilla provisional

--- Desarrollo/BulletTest/BulletTest/Motor/GraphicEngine.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/GraphicEngine.cpp
@@ -73,7 +73,10 @@ void GraphicEngine::setActiveCamera(int ID)
 void GraphicEngine::renderAll()
 {
 	irrDriver->beginScene(true, true, SColor(255, 100, 101, 140));
+
+
 	irrScene->drawAll();
+	irrGUI->drawAll();
 	irrDriver->endScene();
 }
 
@@ -86,6 +89,10 @@ void GraphicEngine::inicializar()
 	irrGUI = irrDevice->getGUIEnvironment();
 	irrScene = irrDevice->getSceneManager();
 	irrDriver = irrDevice->getVideoDriver();
+
+	irrGUI->addImage(irrDriver->getTexture("../media/mirilla.png"),
+		position2d<int>(800/2 - 220/2, 600/2 - 165/2));
+
 
 	irrDevice->getCursorControl()->setVisible(0);
 }
