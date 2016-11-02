0947fcdf0ed5f0304ac191d4a1fc8078906d05b2
Juliyo <julio17795@hotmail.com>
2016-11-02 16:15:29 +0100

    Añadida clase Enemigo, se crea igual que el player

---
+++ Desarrollo/BulletTest/BulletTest/Entities/Enemy.cpp
@@ -0,0 +1,70 @@
+
+#include "Enemy.h"
+#include "../Motor/PhysicsEngine.h"
+#include "../Motor/GraphicEngine.h"
+
+Enemy::Enemy() : Entity(-1, NULL)
+{
+}
+
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::inicializar()
+{
+}
+
+void Enemy::update(Time elapsedTime)
+{
+
+}
+
+void Enemy::handleInput()
+{
+}
+
+void Enemy::cargarContenido()
+{
+	//Creas el nodo(grafico)
+	m_nodo = GraphicEngine::i().createNode(Vec3<float>(200, 500, 0), Vec3<float>(100.f, 100.f, 100.f), "../media/ice0.jpg");
+
+	m_renderState.setPosition(Vec3<float>(200, 500, 0));
+
+	//Al mundo de fisicas del cliente añadimos una representacion del objeto fisico pero no calcula fisicas
+	//(servira para hacer los raycast)
+
+	m_rigidBody = PhysicsEngine::createBoxRigidBody(this, Vec3<float>(100.f, 100.f, 100.f),0.0f, DISABLE_SIMULATION);
+}
+
+void Enemy::borrarContenido()
+{
+
+	//GraphicEngine::i().removeNode(m_nodo);
+}
+
+//Teletransporta un enemigo a la posicion que le pasas
+void Enemy::setPosition(Vec3<float> pos) {
+	m_renderState.setPosition(pos);
+	btTransform transform = m_rigidBody->getCenterOfMassTransform();
+	transform.setOrigin(btVector3(pos.getX(), pos.getY(), pos.getZ()));
+	m_rigidBody->setCenterOfMassTransform(transform);
+	m_nodo->setPosition(pos);
+
+}
+
+/*void Enemy::updateEnemigo(Vec3<float> pos) {
+	m_renderState.updatePositions(pos);
+	btTransform transform = m_rigidBody->getCenterOfMassTransform();
+	transform.setOrigin(btVector3(pos.getX(), pos.getY(), pos.getZ()));
+	m_rigidBody->setCenterOfMassTransform(transform);
+}*/
+
+void Enemy::handleMessage(const Message & message)
+{
+	/*if (message.mensaje == "MOVE") {
+		TPlayer* p = (TPlayer*)message.data;
+		updateEnemigo(p->position);
+	}*/
+}
