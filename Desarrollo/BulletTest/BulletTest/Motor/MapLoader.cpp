05bfeb4023fdf1357cc4099541ec03e3791909d5
kikonster <kike.molto@gmail.com>
2016-12-03 19:54:05 +0100

    ya no peta, leemos el mapa al inicializar

--- Desarrollo/BulletTest/BulletTest/Motor/MapLoader.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/MapLoader.cpp
@@ -6,9 +6,14 @@ using json = nlohmann::json;
 
 void MapLoader::readMap(const std::string & name)
 {	
-	std::ifstream i("rust_export.json");
-	json j;
-	i >> j;
-	std::cout << j.dump() << std::endl;
+	std::ifstream i("../media/terminal_export.json");
+	if(i){
+		json j = json::parse(i);
+
+		for (json::iterator it = j.begin(); it != j.end(); ++it) {
+			std::cout << *it << '\n';
+		}
+	}
+	//std::cout << j.dump() << std::endl;
 }
 
