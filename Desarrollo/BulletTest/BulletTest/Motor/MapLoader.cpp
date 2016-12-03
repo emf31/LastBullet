03210f902b7be7e7dd79f5aaa029d6e7d3fba5ad
kikonster <kike.molto@gmail.com>
2016-12-03 20:06:33 +0100

    Ya parseamos el json

--- Desarrollo/BulletTest/BulletTest/Motor/MapLoader.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/MapLoader.cpp
@@ -11,7 +11,13 @@ void MapLoader::readMap(const std::string & name)
 		json j = json::parse(i);
 
 		for (json::iterator it = j.begin(); it != j.end(); ++it) {
-			std::cout << *it << '\n';
+			
+			json obj = *it;
+			std::cout << "----------------------------"<<'\n';
+			std::cout << "Nombre " << obj["nombre"] << '\n';
+			std::cout << "Posicion "<<obj["posX"] <<','<< obj["posY"] <<  ','<< obj["posZ"] << '\n';
+			std::cout << "Rotacion " << obj["rotX"] << ',' << obj["rotY"] << ',' << obj["rotZ"] << '\n';
+			std::cout << "Escalado " << obj["sizeX"] << ',' << obj["sizeY"] << ',' << obj["sizeZ"] << '\n';
 		}
 	}
 	//std::cout << j.dump() << std::endl;
