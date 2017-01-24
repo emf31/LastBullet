e7bbcce6ffd654c47c3c667e801283058f54181e
kikonster <kike.molto@gmail.com>
2017-01-24 17:26:41 +0100

    ahora lo de la frecuencia tiene sentido

--- Desarrollo/BulletTest/BulletTest/Motor/CameraShake.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/CameraShake.cpp
@@ -11,7 +11,7 @@ void CameraShake::update()
 		//x=A*sen(wt)
 		//T=1/w
 		//t=1.5T
-		if (m_shakeTime.getElapsedTime().asSeconds() < (6*(1 / m_frecuencia))) {
+		if (m_shakeTime.getElapsedTime().asSeconds() < (1.5*(2*PI / m_frecuencia))) {
 			posicion = -m_amplitud*sin(m_frecuencia*m_shakeTime.getElapsedTime().asSeconds());
 			m_camera.setTarget(Vec3<float>(m_camera.getTarget().getX(), m_camera.getTarget().getY() + posicion, m_camera.getTarget().getZ()));
 		}
