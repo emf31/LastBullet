c50e9186e73a14ee0c1cc84b761faa3352625fcf
kikonster <kike.molto@gmail.com>
2017-01-26 14:17:10 +0100

    Ahora baja el arma

--- Desarrollo/BulletTest/BulletTest/Motor/GunRecoil.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/GunRecoil.cpp
@@ -12,6 +12,8 @@ void GunRecoil::RecoilOn()
 {
 	m_recoilActive = true;
 	m_recoilTime.restart();
+	/**/
+//randomAcumulado=Vec3<float>(0,0,0);
 	/*if (static_cast<Player*>(EntityManager::i().getEntity(PLAYER))->getCurrentWeaponName() == "Asalto") {
 		m_amplitud = 0.4;
 	}
@@ -25,10 +27,11 @@ void GunRecoil::RecoilOn()
 
 void GunRecoil::update()
 {
+	int cont = 0;
 	if (m_recoilActive) {
 		if (m_recoilTime.getElapsedTime().asSeconds() < 0.1f) {
-
-			srand(m_recoilTime.getElapsedTime().asMicroseconds());
+			//printf("entro1\n");
+			srand(++cont);
 			int randomX = rand() % 100 +1;
 			int randomY = rand() % 100 +1;
 			int randomZ = rand() % 100 +1;
@@ -46,14 +49,28 @@ void GunRecoil::update()
 			int signoZ = (rand() % 3) -1;//devuelve 0 o 1
 
 
-			Vec3<float> randomf = Vec3<float>(signoX*(float(randomX)/700),signoY*(float(randomY)/600),signoZ*(float(randomZ)/700));
+			Vec3<float>  randomf = Vec3<float>(signoX*(float(randomX)/700),signoY*(float(randomY)/600),signoZ*(float(randomZ)/700));
+			randomStack.push(randomf);
 
 			m_camera.setTarget(Vec3<float>(m_camera.getTarget().getX()+ randomf.getX(), m_camera.getTarget().getY() + randomf.getY(), m_camera.getTarget().getZ()+randomf.getZ()));
 
 
 		}
+		else if(m_recoilTime.getElapsedTime().asSeconds() < 0.2f && !randomStack.empty()){
+			//Vec3<float>  pop = randomStack.top();
+			//printf("entro2\n");
+			//std::cout << randomStack.size() << std::endl;
+			m_camera.setTarget(Vec3<float>(m_camera.getTarget().getX() - randomStack.top().getX(), m_camera.getTarget().getY() - randomStack.top().getY(), m_camera.getTarget().getZ() - randomStack.top().getZ()));
+			randomStack.pop();
+			
+			
+		}
 		else {
 			m_recoilActive = false;
+			while (!randomStack.empty()) {
+				randomStack.pop();
+			}
+
 		}
 
 	}
