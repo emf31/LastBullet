06371521f6e216ca392360efe9b1f6d61eb87b57
kikonster <kike.molto@gmail.com>
2017-01-26 12:07:07 +0100

    ahora retroceso hacia arriba

--- Desarrollo/BulletTest/BulletTest/Motor/GunRecoil.cpp
+++ Desarrollo/BulletTest/BulletTest/Motor/GunRecoil.cpp
@@ -35,19 +35,18 @@ void GunRecoil::update()
 
 			//std::cout << randomX << " " << randomY << " " << randomZ<<std::endl;
 
-			int signoX = rand() % 2;//devuelve 0 o 1
-			if (signoX == 0)
-				signoX--;
+			int signoX = (rand() % 3)-1;//devuelve 0 o 1
+			//if (signoX == 0)
+			//	signoX--;
 
 			int signoY = rand() % 2;//devuelve 0 o 1
-			if (signoY == 0)
-				signoY--;
+			//if (signoY == 0)
+				//signoY--;
 
-			int signoZ = rand() % 2;//devuelve 0 o 1
-			if (signoZ == 0)
-				signoZ--;
+			int signoZ = (rand() % 3) -1;//devuelve 0 o 1
 
-			Vec3<float> randomf = Vec3<float>(signoX*(float(randomX)/700),signoY*(float(randomY)/700),signoZ*(float(randomZ)/700));
+
+			Vec3<float> randomf = Vec3<float>(signoX*(float(randomX)/700),signoY*(float(randomY)/600),signoZ*(float(randomZ)/700));
 
 			m_camera.setTarget(Vec3<float>(m_camera.getTarget().getX()+ randomf.getX(), m_camera.getTarget().getY() + randomf.getY(), m_camera.getTarget().getZ()+randomf.getZ()));
 
