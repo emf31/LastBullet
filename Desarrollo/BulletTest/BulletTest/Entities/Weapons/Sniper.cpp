cd28616391d06878015a14b78c41b1e473729701
kikonster <kike.molto@gmail.com>
2017-02-26 20:05:09 +0100

    Sniper provisional

--- Desarrollo/BulletTest/BulletTest/Entities/Weapons/Sniper.cpp
+++ Desarrollo/BulletTest/BulletTest/Entities/Weapons/Sniper.cpp
@@ -65,7 +65,7 @@ void Sniper::cargarContenido()
 {
 
 	Vec3<float> player_pos = m_ent->getRenderState()->getPosition();
-	m_nodo = GraphicEngine::i().createAnimatedNode(Vec3<float>(player_pos.getX(), player_pos.getY(), player_pos.getZ()), Vec3<float>(0.5f, 0.5f, 0.5f), "", "../media/arma/Asalto.obj");
+	m_nodo = GraphicEngine::i().createAnimatedNode(Vec3<float>(player_pos.getX(), player_pos.getY(), player_pos.getZ()), Vec3<float>(0.1f, 0.1f, 0.1f), "", "../media/arma/Sniper3.obj");
 	m_nodo->setVisible(false);
 
 
